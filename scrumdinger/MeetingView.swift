a3a6f08126d64e67e90999dc0ff902886f1da691
eyb3r <eyber@redpoll.it>
2023-08-28 22:40:43 +0200

    Initial Commit

---
+++ scrumdinger/MeetingView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  scrumdinger
+//
+//  Created by Mateusz Lewicki on 28/08/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                ProgressView(value: 10, total: 15)
+                HStack {
+                    VStack {
+                        Text("Seconds Elapsed")
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                    VStack {
+                        Text("Seconds Remaining")
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Mateusz Lewicki on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
